9737144e0777759ba29b98d9637c8c89a5b58572
Junsung Lim <wns349@kaist.ac.kr>
2015-02-23 15:41:36 +0900

    commit ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 // GET /
 router.get('/', function(req, res) {
   res.render('index');
+});
+
+router.get('/temp', function(req, res){
+  fs.readdir('D:\\Temp\\', function (err, files){
+    if(err){
+      throw err;
+    }
+    
+    res.render('temp', {files: files});
+  });
 });
 
 module.exports = router;
